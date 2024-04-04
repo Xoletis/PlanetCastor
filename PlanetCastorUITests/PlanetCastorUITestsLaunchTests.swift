631fb47e853f1332b9e63c7e9cff6068238e0c48
xoletis <spotedcarrot@gmail.com>
2024-04-04 14:34:01 +0200

    Initial Commit

---
+++ PlanetCastorUITests/PlanetCastorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PlanetCastorUITestsLaunchTests.swift
+//  PlanetCastorUITests
+//
+//  Created by Ivan Rocque on 04/04/2024.
+//
+
+import XCTest
+
+final class PlanetCastorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
