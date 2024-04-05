dbbc2ef68e5f55127ade5202a480cec3435a4823
AnNaNnq <anna.sarbiewski@gmail.com>
2024-04-05 16:34:34 +0200

    shadows

--- PlanetCastor/ViewController.swift
+++ PlanetCastor/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let backImage = UIImage(named: "boutonRetour")
+        self.navigationController?.navigationBar.backIndicatorImage = backImage
+        self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = backImage
+        
+        for button in buttons {
+            button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.40).cgColor
+            button.layer.shadowOpacity = 0.9
+            button.layer.shadowOffset = CGSize(width: 1.5, height: 4.0)
+            button.layer.shadowRadius = 2
+            button.layer.cornerRadius = 4.0
+        }
     }
-
+    
 }
 
