6840bd04bd636a1a22b2404f04f52b384c633d61
AnNaNnq <anna.sarbiewski@gmail.com>
2024-04-08 12:52:47 +0200

    premier test de bouton cliqués

--- PlanetCastor/PlaneteParameter.swift
+++ PlanetCastor/PlaneteParameter.swift
@@ -9,22 +9,56 @@ import Foundation
 import UIKit
 
 class PlaneteParameter: UIViewController {
+      
     
     @IBOutlet var ButtonsTypePlanet: [UIButton]!
+    @IBOutlet var ButtonsAtmosphere: [UIButton]!
+    @IBOutlet var ButtonsRessources: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        for button in ButtonsTypePlanet {
-            button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.40).cgColor
-            button.layer.shadowOpacity = 0.9
-            button.layer.shadowOffset = CGSize(width: 1.5, height: 4.0)
-            button.layer.shadowRadius = 2
-            button.layer.cornerRadius = 4.0
+        if ButtonsTypePlanet != nil {
+            for button in ButtonsTypePlanet {
+                button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.40).cgColor
+                button.layer.shadowOpacity = 0.9
+                button.layer.shadowOffset = CGSize(width: 1.5, height: 4.0)
+                button.layer.shadowRadius = 2
+                button.layer.cornerRadius = 4.0
+            }
         }
-        
+        if ButtonsAtmosphere != nil {
+            for button in ButtonsAtmosphere {
+                button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.40).cgColor
+                button.layer.shadowOpacity = 0.9
+                button.layer.shadowOffset = CGSize(width: 1.5, height: 4.0)
+                button.layer.shadowRadius = 2
+                button.layer.cornerRadius = 16
+            }
+        }
+        if ButtonsRessources != nil {
+            for button in ButtonsRessources {
+                button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.40).cgColor
+                button.layer.shadowOpacity = 0.9
+                button.layer.shadowOffset = CGSize(width: 1.5, height: 4.0)
+                button.layer.shadowRadius = 2
+                button.layer.cornerRadius = 4.0
+            }
+        }
     }
     
+    
+    @IBAction func ClickAtmosphereButton(_ sender: UIButton) {
+        if sender.layer.borderColor == CGColor(red: 217/255, green: 169/255, blue: 255/255, alpha: 1){
+            sender.configuration?.baseForegroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1);
+            sender.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0)
+            sender.layer.borderWidth = 1
+        } else {
+            sender.configuration?.baseForegroundColor = UIColor(red: 217/255, green: 169/255, blue: 255/255, alpha: 1);
+            sender.layer.borderColor = CGColor(red: 217/255, green: 169/255, blue: 255/255, alpha: 1)
+            sender.layer.borderWidth = 3
+            sender.layer.cornerCurve = .continuous
+        }
+    }
     
     @IBAction func ClickOnTerrestreButton(_ sender: UIButton) {
         for button in ButtonsTypePlanet{
