95722365bdb6c4c704db0b19f842c16960a48082
AnNaNnq <anna.sarbiewski@gmail.com>
2024-04-10 12:25:23 +0200

    popover

--- PlanetCastor/ImageParameter.swift
+++ PlanetCastor/ImageParameter.swift
@@ -10,7 +10,15 @@ import UIKit
 
 class ImageParameter: UIViewController {
         
+    @IBOutlet weak var plusButton: UIButton!
+    
     override func viewDidLoad() {
             super.viewDidLoad()
+        
+        plusButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.40).cgColor
+        plusButton.layer.shadowOpacity = 0.9
+        plusButton.layer.shadowOffset = CGSize(width: 1.5, height: 4.0)
+        plusButton.layer.shadowRadius = 2
+        plusButton.layer.cornerRadius = 4.0
     }
 }
